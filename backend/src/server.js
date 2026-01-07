ff021f70421131ae5cfe3b0b289ee0389e054256
Begeeeg <v.yecyec7@gmail.com>
2026-01-07 17:57:42 +0800

    remove env

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,11 @@
 import express from "express";
 import noteRoutes from "./routes/noteRoutes.js";
+import { connectDB } from "./config/db.js";
 
 const app = express();
 const PORT = 5000;
 
-// Middleware to parse JSON
-app.use(express.json());
+connectDB();
 
 // Routes
 app.use("/api/notes", noteRoutes);
@@ -13,5 +13,3 @@ app.use("/api/notes", noteRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-
-// mongodb+srv://vyecyec7_db_user:mj9cfNBjgG9dGp3s@cluster0.bmz0vhw.mongodb.net/?appName=Cluster0
