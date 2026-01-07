fa89ab89a0078401c9b7cd6fc0af26dae8bddfd5
Begeeeg <v.yecyec7@gmail.com>
2026-01-07 18:09:14 +0800

    dotenv, mongoose

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,15 @@
 import express from "express";
 import noteRoutes from "./routes/noteRoutes.js";
 import { connectDB } from "./config/db.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 connectDB();
 
-// Routes
 app.use("/api/notes", noteRoutes);
 
 app.listen(PORT, () => {
