3044292808fe8772d4629083a492a2045d7e474a
Begeeeg <v.yecyec7@gmail.com>
2026-01-07 15:48:31 +0800

    nodemon

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,15 @@
 import express from "express";
 
 const app = express();
-
 const PORT = 5000;
+
+app.get("/api/notes", (req, res) => {
+  res.status(200).send("Here are your notes");
+});
+
+app.get("/*", (req, res) => {
+  res.status(404).send("Route not found");
+});
 
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
