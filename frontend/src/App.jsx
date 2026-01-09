c85d3db72d66255ff9d538e6fe4b3e78dc913023
Begeeeg <v.yecyec7@gmail.com>
2026-01-09 09:44:50 +0800

    tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,28 @@
 import React from "react";
+import { Routes, Route } from "react-router";
+import HomePage from "./pages/homePage";
+import CreatePage from "./pages/createPage";
+import NotePage from "./pages/notePage";
+import toast from "react-hot-toast";
 
-export const App = () => {
-  return <div>App</div>;
+const App = () => {
+  return (
+    <div>
+      <button
+        onClick={() => toast.success("This is a success message!")}
+        className="text-red-500 p-2 bg-blue-700"
+      >
+        Show Toast
+      </button>
+      <button className="text-blue-500 p-2 bg-black">Check 1</button>
+      <button className="text-blue-500 p-2 bg-black">Check 2</button>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/create" element={<CreatePage />} />
+        <Route path="/note/:id" element={<NotePage />} />
+      </Routes>
+    </div>
+  );
 };
+
+export default App;
