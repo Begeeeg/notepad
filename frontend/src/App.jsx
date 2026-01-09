eedcbc71047f5a0fe4cd5c507e2263a7e8806a4e
Begeeeg <v.yecyec7@gmail.com>
2026-01-09 10:21:58 +0800

    initial frontend setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,19 +3,10 @@ import { Routes, Route } from "react-router";
 import HomePage from "./pages/homePage";
 import CreatePage from "./pages/createPage";
 import NotePage from "./pages/notePage";
-import toast from "react-hot-toast";
 
 const App = () => {
   return (
-    <div>
-      <button
-        onClick={() => toast.success("This is a success message!")}
-        className="text-red-500 p-2 bg-blue-700"
-      >
-        Show Toast
-      </button>
-      <button className="text-blue-500 p-2 bg-black">Check 1</button>
-      <button className="text-blue-500 p-2 bg-black">Check 2</button>
+    <div data-theme="coffee">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
